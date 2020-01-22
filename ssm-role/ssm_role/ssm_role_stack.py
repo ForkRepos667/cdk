c27db3c39df7572550c9fc53cb9b0a5bb7131e4b
Philip Horrocks <philiphorrocks@Philips-MBP.lan>
2020-01-22 22:03:59 +0000

    v0.1

--- ssm-role/ssm_role/ssm_role_stack.py
+++ ssm-role/ssm_role/ssm_role_stack.py
@@ -1,9 +1,32 @@
-from aws_cdk import core
+import json
+from aws_cdk import (
+    aws_iam as iam,
+    core,
+)
 
+
+iam_role_properties={'':''}
 
 class SsmRoleStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+    #################################################
+    #
+    # Create IAM role for ssm patching
+    # 
+    #################################################
+            
+            newRole = iam.Role(self, 'ssmCustomRole',
+                            assumed_by=iam.ServicePrincipal("s3.amazonaws.com"),
+                            role_name='ssmCustomRole'
+                            )
+            
+            [newRole.add_to_policy(iam.PolicyStatement(resources=[key],actions=[value])) for key, value in iam_role_properties.items()]
+        
+        except:
+            
+            print('[-] Failed to execute AWS custome resource for S3 bucket')
+            
+ 
